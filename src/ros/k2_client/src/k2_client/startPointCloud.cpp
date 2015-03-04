1fb31d3523fbabc66f38c8959a3c2ea3512725d5
Somahtr <somahtr@aol.com>
2015-03-04 18:39:03 +0000

    Implemented point cloud deserialiser. Succeeded in streaming point cloud over network

--- src/ros/k2_client/src/k2_client/startPointCloud.cpp
+++ src/ros/k2_client/src/k2_client/startPointCloud.cpp
@@ -1,6 +1,7 @@
 #include "k2_client.h"
 #include <pcl_ros/point_cloud.h>
 #include <pcl/point_types.h>
+using namespace std;
 
 typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
 
@@ -19,25 +20,41 @@ int main(int argC,char **argV)
 	n.getParam("/serverNameOrIP",serverAddress);
 	Socket mySocket(serverAddress.c_str(),"9005",streamSize);
 	
-	ros::Publisher pub = n.advertise<PointCloud>("pointCloud",1);
+	ros::Publisher pub = n.advertise<PointCloud>("point_cloud",1);
     
-    PointCloud:Ptr pc (new PointCloud);
-    pc->resize(512,424);
-     
+    PointCloud::Ptr pc (new PointCloud);
+    
+    pc->header.frame_id =  ros::this_node::getNamespace().substr(1,std::string::npos) + "/kinect_pcl"; 
 	while(ros::ok())
 	{
+	    //pc->width = 512*424;
+        //pc->height = 1;
+        
 	    cout << "Reading data..." << endl;
 		mySocket.readData();
 		
 		cout << "Copying data..." << endl;
-		memcpy(&pc->points,&mySocket.mBuffer, pointBufferSize);
+		uint64_t ptr = 0;
+		float x,y,z;
+		while(ptr<pointBufferSize)
+		{
+		    memcpy(&x, &mySocket.mBuffer[ptr  ],4);
+		    memcpy(&y, &mySocket.mBuffer[ptr+4],4);
+		    memcpy(&z, &mySocket.mBuffer[ptr+8],4);
+		    
+		    pc->push_back(pcl::PointXYZ(x,y,z));
+		    //cout << "x: " << x << " y: " << y << " z: " << z << endl;
+		    ptr += 12;
+		}
 		
 		double utcTime;
 		memcpy(&utcTime,&mySocket.mBuffer[pointBufferSize],sizeof(double));
-		pc.header.stamp = ros::Time(utcTime);
+		pc->header.stamp = ros::Time(utcTime).toSec();
 
-        cout << "Publishing point cloud" << endl;
+        cout << "Publishing point cloud w" << pc->width << " h " << pc->height << endl;
 		pub.publish(pc);
+		pc->clear();
+		
 		ros::spinOnce();
 	}
 	return 0;
