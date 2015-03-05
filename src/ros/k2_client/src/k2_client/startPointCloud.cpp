8afd858200f1ec2cbb447b50d18beb174c10b6cc
Rich Wareham <rjw57@cam.ac.uk>
2015-03-05 14:06:15 +0000

    remove trailing whitespace and fix indentation

--- src/ros/k2_client/src/k2_client/startPointCloud.cpp
+++ src/ros/k2_client/src/k2_client/startPointCloud.cpp
@@ -12,41 +12,41 @@ int main(int argC,char **argV)
 {
 	ros::init(argC,argV,"startPointCloud");
 	ros::NodeHandle n;
-	
+
 	std::string serverAddress;
 	n.getParam("/serverNameOrIP",serverAddress);
 	Socket mySocket(serverAddress.c_str(),"9005",streamSize);
-	
+
 	ros::Publisher pub = n.advertise<PointCloud>("point_cloud",1);
-    
+
     PointCloud::Ptr pc (new PointCloud);
-    
-    pc->header.frame_id =  ros::this_node::getNamespace().substr(1,std::string::npos) + "/kinect_pcl"; 
+
+    pc->header.frame_id =  ros::this_node::getNamespace().substr(1,std::string::npos) + "/kinect_pcl";
 	while(ros::ok())
-	{        
+	{
 	    cout << "Reading data..." << endl;
 		mySocket.readData();
-		
+
 		cout << "Copying data..." << endl;
 		uint64_t ptr = 0;
 		float x,y,z;
 		while(ptr<pointBufferSize)
 		{
-		    memcpy(&x, &mySocket.mBuffer[ptr  ],4);
-		    memcpy(&y, &mySocket.mBuffer[ptr+4],4);
-		    memcpy(&z, &mySocket.mBuffer[ptr+8],4);
+			memcpy(&x, &mySocket.mBuffer[ptr  ],4);
+			memcpy(&y, &mySocket.mBuffer[ptr+4],4);
+			memcpy(&z, &mySocket.mBuffer[ptr+8],4);
 
-		    pc->push_back(pcl::PointXYZ(x,y,z));
-		    ptr += 12;
+			pc->push_back(pcl::PointXYZ(x,y,z));
+			ptr += 12;
 		}
-		
+
 		double utcTime;
 		memcpy(&utcTime,&mySocket.mBuffer[pointBufferSize],sizeof(double));
 		pc->header.stamp = ros::Time(utcTime).toSec();
 
 		pub.publish(pc);
 		pc->clear();
-		
+
 		ros::spinOnce();
 	}
 	return 0;
